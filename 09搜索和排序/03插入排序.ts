7b45a83c65d0518e5094f202a9f5a1464917e5bb
wp19921010 <kunpeng@wshoto.com>
2022-01-05 15:37:40 +0800

    feat: 插入排序

--- 09搜索和排序/03插入排序.ts
+++ 09搜索和排序/03插入排序.ts
@@ -5,4 +5,18 @@
  */
 const arr93: number[] = [2, 4, 5, 3, 1];
 
+for (let i = 1; i < arr93.length; i += 1) {
+    const temp = arr93[i];
+    let j = i;
+    while (j > 0) {
+        if (arr93[j - 1] > temp) {
+            arr93[j] = arr93[j - 1]
+        } else {
+            break
+        }
+        j -= 1;
+    }
+    arr93[j] = temp;
+}
+
 console.log(arr93)
