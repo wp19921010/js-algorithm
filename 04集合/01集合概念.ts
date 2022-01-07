051e9dfb9b602f3d5e3c4b7ce8a64eac1b53c7b9
wp19921010 <kunpeng@wshoto.com>
2022-01-07 15:14:36 +0800

    feat(set): 集合概念

--- 04集合/01集合概念.ts
+++ 04集合/01集合概念.ts
@@ -1,11 +1,14 @@
 // 去重
-const arr = [1, 1, 2, 2];
-const arr2 = [...new Set(arr)];
+const arr41 = [1, 1, 2, 2];
+const arr42 = [...new (Set as any)(arr41)];
+console.log(arr42);
 
 // 判断元素是否在集合中
-const set = new Set(arr);
+const set = new Set(arr41) as any;
 const has = set.has(3);
+console.log(has);
 
 // 求交集
 const set2 = new Set([2, 3]);
 const set3 = new Set([...set].filter(item => set2.has(item)));
+console.log(set3);
