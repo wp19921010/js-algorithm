334533130947262cd04a69779e4a1ce9af9ddc62
wp19921010 <kunpeng@wshoto.com>
2022-01-07 15:35:30 +0800

    feat(map): 新建文件

--- 04集合/01集合概念.ts
+++ 04集合/01集合概念.ts
@@ -1,14 +1,16 @@
 // 去重
-const arr41 = [1, 1, 2, 2];
-const arr42 = [...new (Set as any)(arr41)];
+const arr41 = [1, 2, 2, 2];
+// @ts-ignore
+const arr42 = [...new Set(arr41)];
 console.log(arr42);
 
 // 判断元素是否在集合中
-const set = new Set(arr41) as any;
+const set = new Set(arr41);
 const has = set.has(3);
 console.log(has);
 
 // 求交集
 const set2 = new Set([2, 3]);
+// @ts-ignore
 const set3 = new Set([...set].filter(item => set2.has(item)));
 console.log(set3);
